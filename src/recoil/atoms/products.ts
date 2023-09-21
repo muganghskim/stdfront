0b92f94ea63ceb9924033b0923c72cf0f64c6167
kimhs <rhgustmfrh@naver.com>
2023-09-21 17:59:30 +0900

    "리코일 product 수정"

--- src/recoil/atoms/products.ts
+++ src/recoil/atoms/products.ts
@@ -1,54 +1,64 @@
 /* 백엔드 데이터 필요 pdName categoryName pdDetail pdPrice pdStat pdSize pdImg pdQuantity*/
 
-// import { atom } from "recoil";
-// import axios from "axios";
+import { atom } from "recoil";
+import axios from "axios";
 
-// interface Product {
-//   id: number;
-//   name: string;
-//   description: string;
-//   price: string;
-//   imageUrl: string;
-// }
+interface Product {
+  id: string;
+  name: string;
+  category: string;
+  detail: string;
+  price: number;
+  stat: string;
+  size: string;
+  imgUrl: string;
+}
 
-// interface EnrollData {
-//   name: string;
-//   description: string;
-//   price: string;
-//   image: File | null;
-// }
+interface EnrollData {
+  userEmail: string;
+  pdName: string;
+  categoryName: string;
+  pdDetail: string;
+  pdPrice: string;
+  pdStat: string;
+  pdSize: string;
+  pdQuantity: string;
+  image: File | null;
+}
 
-// // 전체 조회
-// export const getAllProducts = async (): Promise<Product[]> => {
-//   const response = await axios.get("http://localhost:8080/products");
-//   return response.data;
-// };
+// 전체 조회
+export const getAllProducts = async (): Promise<Product[]> => {
+  const response = await axios.get("http://localhost:8096/api/products");
+  return response.data;
+};
 
-// // 상세 페이지 조회
-// export const getProductById = async (id: number): Promise<Product> => {
-//   const response = await axios.get(`http://localhost:8080/products/${id}`);
-//   return response.data;
-// };
+// 상세 페이지 조회
+export const getProductById = async (productId: number): Promise<Product> => {
+  const response = await axios.get(
+    `http://localhost:8096/api/products/${productId}`
+  );
+  return response.data;
+};
 
-// // 검색 기능
-// export const searchProductsByName = async (
-//   searchName: string
-// ): Promise<{ searchResults: Product[]; notFound: boolean }> => {
-//   try {
-//     const response = await axios.get(
-//       `http://localhost:8080/products/search?searchName=${searchName}`
-//     );
+// 검색 기능
+export const searchProductsByName = async (
+  searchName: string
+): Promise<{ searchResults: Product[]; notFound: boolean }> => {
+  try {
+    const response = await axios.get(
+      `http://localhost:8096/api/productSearch/?searchName=${searchName}`
+    );
 
-//     if (response.status === 200) {
-//       return { searchResults: response.data, notFound: false };
-//     } else {
-//       throw new Error("Product not found");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return { searchResults: [], notFound: true };
-//   }
-// };
+    if (response.status === 200) {
+      return { searchResults: response.data, notFound: false };
+    } else {
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    console.error(error);
+    return { searchResults: [], notFound: true };
+  }
+};
 
 // // 이미지 변경 기능
 // export const updateProductImage = async (
@@ -79,38 +89,43 @@
 // //     return { name, description, price, imageUrl };
 // // };
 
-// export const enroll = async (data: EnrollData) => {
-//   const { name, description, price, image } = data;
-//   const formData = new FormData();
-//   formData.append("name", name);
-//   formData.append("description", description);
-//   formData.append("price", price);
+export const enroll = async (data: EnrollData) => {
+  const {
+    userEmail,
+    pdName,
+    categoryName,
+    pdDetail,
+    pdPrice,
+    pdStat,
+    pdSize,
+    pdQuantity,
+    image
+  } = data;
+  const formData = new FormData();
+  formData.append("userEmail", userEmail);
+  formData.append("pdName", pdName);
+  formData.append("categoryName", categoryName);
+  formData.append("pdDetail", pdDetail);
+  formData.append("pdPrice", pdPrice);
+  formData.append("pdStat", pdStat);
+  formData.append("pdSize", pdSize);
+  formData.append("pdQuantity", pdQuantity);
 
-//   // 이미지가 있는 경우에만 FormData에 추가
-//   if (image) {
-//     formData.append("file", image);
-//   }
+  // 이미지가 있는 경우에만 FormData에 추가
+  if (image) {
+    formData.append("file", image);
+  }
 
-//   const response = await axios.post(
-//     "http://localhost:8080/products/enroll",
-//     formData,
-//     {
-//       headers: {
-//         "Content-Type": "multipart/form-data"
-//       }
-//     }
-//   );
-//   const {
-//     name: resName,
-//     description: resDescription,
-//     price: resPrice,
-//     imageUrl
-//   } = response.data;
+  const response = await axios.post(
+    "http://localhost:8096/api/registerProduct",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }
+  );
+  const resData: Product = response.data;
 
-//   return {
-//     name: resName,
-//     description: resDescription,
-//     price: resPrice,
-//     imageUrl
-//   };
-// };
+  return resData;
+};
